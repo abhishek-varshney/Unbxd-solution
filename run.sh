8c5dfd00efe5aeb9192fa8a211de3ef61bf88f95
abhishek-varshney <abhishekvrshny@gmail.com>
2013-05-06 01:24:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# The script take a search query '30f7594e-1d16-40c9-8a66-8dc641d65187' and prints all products related to the product id in search query. 3rd argument (1 here) indicates that it is search based on search query
-echo "Getting desired results for search query '30f7594e-1d16-40c9-8a66-8dc641d65187'"
+# The script take a search query 'q1' and prints all products related to the product id in search query. 3rd argument (1 here) indicates that it is search based on search query
+echo "Getting desired results for search query 'q1'"
 echo ""
-python extract.py list1.csv list2.csv 1 30f7594e-1d16-40c9-8a66-8dc641d65187
+python extract.py list1.csv list2.csv 1 q1
 echo ""
-echo "Getting desired results for artist Dolly"
+echo "Getting desired results for artist 'a1'"
 echo ""
-# Here the script take an artist 'Dolly' and prints all search queries with which the product of particulat artist was associated. 3rd argument (2 here) indicates that it is search based on artist
-python extract.py list1.csv list2.csv 2 Dolly
+# Here the script take an artist 'a1' and prints all search queries with which the product of particulat artist was associated. 3rd argument (2 here) indicates that it is search based on artist
+python extract.py list1.csv list2.csv 2 a1
