f4399013007f39aec7404bdbff93cc4f974ac0ba
abhishek-varshney <abhishekvrshny@gmail.com>
2013-05-07 02:06:15 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,29 +1,27 @@
 import sys
 import csv
-def load(path):
-  	ret=[]
-	with open(path, 'rb') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',')
-		for row in spamreader:
-			ret.append(row)
-	return ret
-
+#import json
+import simplejson
+from pprint import pprint
 if __name__ == '__main__':
- 
   if (len(sys.argv) != 5):
     print "usage : python extract.py [path to list 1] [path to list 2] [1/2 (Use case)] [Search query/Artist]"
     sys.exit(1)
-  l1 = load (sys.argv[1])
-  l2 = load (sys.argv[2])
-  if sys.argv[3] == "1":
-  	f1=filter(lambda x:x[0]==sys.argv[4],list(l2))
-  	for row in list(f1): 
-  		mapp=filter(lambda x: 1 if x[0]==row[1] else 0,list(l1)) 
-  		for row in list(mapp):
-  			print row
-  if sys.argv[3] == "2":
-  	f1=filter(lambda x:x[3]==sys.argv[4],list(l1))
-  	for row in list(f1): 
-  		mapp=filter(lambda x: 1 if x[1]==row[0] else 0,list(l2)) 
-  		for row in list(mapp):
-  			print row[0]
+  use_case="query" if int(sys.argv[3])==1 else "artist"  
+  if sys.argv[3]=="1":
+  	l2 = simplejson.loads(open(sys.argv[2]).read())
+  	filter_out=filter(lambda x:x[use_case]==sys.argv[4],l2)
+  	del l2
+  	l1 = simplejson.loads(open(sys.argv[1]).read())
+  if sys.argv[3]=="2":
+  	l1 = simplejson.loads(open(sys.argv[1]).read())
+  	filter_out=filter(lambda x:x[use_case]==sys.argv[4],l1)
+  	del l1
+  	l2 = simplejson.loads(open(sys.argv[2]).read())
+  for row in filter_out: 
+  	if sys.argv[3]=="1":
+  		result_set=filter(lambda x: 1 if x["productId"]==row["productId"] else 0,l1)
+  	if sys.argv[3]=="2":
+  		result_set=filter(lambda x: 1 if x["productId"]==row["productId"] else 0,l2)
+  	for row in result_set:
+  		pprint(row)
