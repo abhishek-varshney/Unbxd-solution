7464365d326c1ece2704e9bdd66da61b982a17a1
abhishek-varshney <abhishekvrshny@gmail.com>
2013-05-07 02:00:39 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# The script take a search query 'q1' and prints all products related to the product id in search query. 3rd argument (1 here) indicates that it is search based on search query
-echo "Getting desired results for search query 'q1'"
+# The script take a search query 'Less Than Jake' and prints all products related to the product id in search query. 3rd argument (1 here) indicates that it is search based on search query
+echo "Getting desired results for search query 'Less Than Jake'"
 echo ""
-python extract.py list1.csv list2.csv 1 q1
+python extract.py data.json query.json 1 "Less Than Jake"
 echo ""
-echo "Getting desired results for artist 'a1'"
+echo "Getting desired results for artist 'Trial of the Bow'"
 echo ""
-# Here the script take an artist 'a1' and prints all search queries with which the product of particulat artist was associated. 3rd argument (2 here) indicates that it is search based on artist
-python extract.py list1.csv list2.csv 2 a1
+# Here the script take an artist 'Trial of the Bow' and prints all search queries with which the product of particulat artist was associated. 3rd argument (2 here) indicates that it is search based on artist
+python extract.py data.json query.json 2 "Trial of the Bow"
